7662fe9addfd70a06157ba97f645245c0c4a227b
Ariel Soto <fer.ariel85@gmail.com>
2022-04-13 20:02:29 -0300

    Iniciando proyecto

---
+++ src/main/java/com/iconos/iconos/IconosApplication.java
@@ -0,0 +1,13 @@
+package com.iconos.iconos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IconosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IconosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iconos/iconos/IconosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iconos.iconos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IconosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
